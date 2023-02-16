3273eb37907364f61ed6b579f7ba9fe668fe7fc6
Zaur Tskhvaradze <zaur.tskhvaradze@external.danone.com>
2023-02-16 10:26:01 +0400

    added one more step: last words from sentence + corrected one vvariable name

--- Homework_3.py
+++ Homework_3.py
@@ -15,7 +15,7 @@ homEwork:
 
  
 
-  last iz TO calculate nuMber OF Whitespace characteRS in this Tex. caREFULL, not only Spaces, but ALL whitespaces. I got 87.
+  last iz TO calculate nuMber OF Whitespace characteRS in this Text. caREFULL, not only Spaces, but ALL whitespaces. I got 87.
 """
 
 # I tryied to do this task by braking down the process into small pieces.
@@ -27,18 +27,18 @@ print("Number of whitespaces in the text = ", no_whitespaces)
 # part_2: get all the characters lowercase
 task = task.lower()
 
-# part_3: split the sentence based on whitespaces and get the list of the words
-list = re.split(r'\s+', task)
+# part_3: split the sentence based on whitespaces and get the my_list of the words
+my_list = re.split(r'\s+', task)
 
 # part_4: find the "iz" and replace it with "is"
-for i in list: # Loop through the elements in the list
+for i in my_list: # Loop through the elements in the my_list
     if re.match(r'^i.*?z$', i): # Check if the element matches the pattern '^i.*?z$'
-        index = list.index(i) # When match is found, find the index of the match in the list
-        list[index] = i.replace('z', 's') # Replace 'z' with 's' in the match
+        index = my_list.index(i) # When match is found, find the index of the match in the list
+        my_list[index] = i.replace('z', 's') # Replace 'z' with 's' in the match
 
-# part_5: Join the list and get the whole sentence + add capitalization of the first symbol after period.
+# part_5: Join the my_list and get the whole sentence + add capitalization of the first symbol after period.
 
-task = ' '.join(list) # Join the elements in the list with a space character as the separator
+task = ' '.join(my_list) # Join the elements in the my_list with a space character as the separator
 task = task.strip() # Remove whitespaces from the string
 sentences = task.split(". ") # Split the sentence into separate sentences based on the period
 capitalized = [sentence.capitalize() for sentence in sentences] # Capitalize the first letter of each word in each sentence
@@ -46,3 +46,15 @@ task = ". ".join(capitalized) # Join the sentences back together
 
 # Print the final result
 print(task)
+
+
+# part_6: print last word of every sentence
+sentences = task.split(". ") # split sentence based on full stop 
+last_word_sentence = [] # make empty list to hold last words
+for sentence in sentences: # Loop through each sentence 
+    words = sentence.split()  # Split the sentence into separate words
+    last_word = words[-1] # Get the last word of the sentence
+    last_word_sentence.append(last_word) # Add the last word to the created list
+
+last_word_sentence = " ".join(last_word_sentence) # Join last words together based on whitespace in order to form sentence
+print(f"Sentence created from last words is:", last_word_sentence)
